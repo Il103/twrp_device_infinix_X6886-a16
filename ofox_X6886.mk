b8961b4271b111ea2f9574b29ce67dafb6bdc69c
# B E R U <berucrypt02@gmail.com>
2026-08-14 21:28:53 -0300

    Create  ofox_X6886.mk

---
+++ ofox_X6886.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from Infinix-X6886 device
+$(call inherit-product, device/infinix/X6886/device.mk)
+
+# Product Specifics
+PRODUCT_NAME := ofox_X6886
+PRODUCT_DEVICE := X6886
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X6886
+PRODUCT_MANUFACTURER := INFINIX
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
