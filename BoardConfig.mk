820ff3341a9255e6a643e85a82fff24ee1a6f57e
Youssef (Jo/Beru) <youssef@spider-team.local>
2026-08-29 03:35:05 +0300

    Set MAINTAINER := BERU

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,7 +164,7 @@ TW_NO_LEGACY_PROPS := true
 TW_NO_BIND_SYSTEM := true
 TW_BACKUP_EXCLUSIONS := /data/fonts
 TW_DEVICE_VERSION := Infinix_X6886
-MAINTAINER := Spider Team - Youssef (Jo/Beru)
+MAINTAINER := BERU
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
 TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
 
